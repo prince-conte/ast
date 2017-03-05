7e4e28994a21ea1fd7f5d21347709fef1f715d32
prince-conte <prince-conte@outlook.com>
2017-03-05 23:02:19 +0300

    init

---
+++ tars-master/markup/components/index-partners/index-partners.js
@@ -0,0 +1,8 @@
+$('.index-partners__grid').slick({
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 4000,
+	prevArrow: '<i data-role="none" class="slick-nav slick-prev" aria-label="Previous" tabindex="0" role="button"></i>',
+  nextArrow: '<i data-role="none" class="slick-nav slick-next" aria-label="Next" tabindex="0" role="button"></i>',
+});
---
+++ tars-master/markup/static/js/main.js
@@ -0,0 +1,8 @@
+// Vendors
+require libraries/jquery.js
+require plugins/slick.js
+
+
+// Components
+
+require components/index-partners/index-partners.js
