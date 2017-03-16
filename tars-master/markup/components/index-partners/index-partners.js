6a79d01984d50b392a4e0d620f0816a4ebde6695
prince-conte <prince-conte@outlook.com>
2017-03-16 21:45:23 +0300

    resp(1/5)

--- tars-master/markup/components/index-partners/index-partners.js
+++ tars-master/markup/components/index-partners/index-partners.js
@@ -5,4 +5,16 @@ $('.index-partners__grid').slick({
   autoplaySpeed: 4000,
 	prevArrow: '<i data-role="none" class="slick-nav slick-prev" aria-label="Previous" tabindex="0" role="button"></i>',
   nextArrow: '<i data-role="none" class="slick-nav slick-next" aria-label="Next" tabindex="0" role="button"></i>',
+	responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
