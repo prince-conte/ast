e455e6e6a8929afae1fffca1940d2b16b367f5fc
prince-conte <prince-conte@outlook.com>
2017-03-23 22:35:00 +0300

    +news-article

--- tars-master/markup/components/jobs-info/jobs-info.js
+++ tars-master/markup/components/jobs-info/jobs-info.js
@@ -3,13 +3,13 @@ var $heightNav = $('nav').outerHeight();
 
 $('.js-accordionJobs').on('click' , function(){
 
-    if ($(this).closest('.js-accordionJobs').hasClass('is-active')) {
+    if ($(this).closest('.jobs-info__vacancy').hasClass('is-active')) {
 			
-        $(this).closest('.js-accordionJobs').removeClass('is-active');
+        $(this).closest('.jobs-info__vacancy').removeClass('is-active');
     }
 
     else {
-        $(this).closest('.js-accordionJobs').addClass('is-active');
+        $(this).closest('.jobs-info__vacancy').addClass('is-active');
 				var destination = $(this).closest('.js-accordionJobs').offset().top - $heightNav;
 				jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 800);
 				return false;
